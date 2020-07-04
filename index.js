56aa925a7464a78ac681634114dd6d7cf3f4d9e3
sristi <emailsristi@gmail.com>
2020-07-05 00:09:56 +0530

    clean up

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-//variables
-var winningCombination=[-1,-1,-1];
+/////////////////variables///////////////////////////////
 var resetbutton=document.getElementById('play-again');
 var boardWrap=document.querySelector('#board_wrap');
 var board=document.querySelector('#board');
 var allSquares=document.querySelectorAll('.square');
 var winnerDisplay=document.getElementById('winner');
 var winningMessege=document.querySelector('.winning-messege');
+var squares= document.querySelectorAll(".square");
+
+var winningCombination=[-1,-1,-1];
 var filled=0;
 var movesPlayed=0;
+var player1='X';
+var player2='O';
+var currentPlayer=1;
 var winningCombos=
 [
     [0,1,2],
@@ -19,81 +24,70 @@ var winningCombos=
     [0,4,8],
     [2,4,6]
 ];
+////////////////////////////////////////////////////////////
 
 
-// Event Listeners
+
+/////////////////// Event Listeners////////////////////////
+
 resetbutton.addEventListener('click',resetBoard);
+for (var i = 0 ; i < squares.length; i++) {
+    squares[i].addEventListener('click' , playerMoved ) ; 
+}
+
+///////////////////////////////////////////////////////////
 
 
+////////////////////Functions/////////////////////////////
 
-//Functions
-function resetBoard(){
+function resetBoard()
+{
     allSquares.forEach(resetToNull);
     winnerDisplay.innerHTML="";
     movesPlayed=0;
     winningMessege.classList.remove("show-winning-messege");
 }
-function resetToNull(item){
-        item.innerHTML="";
-        for(var i=0;i<3;i++)
-            winningCombination[i]=-1;
-        item.classList.remove("blur","not-allowed","unhover","highlight-winning-square");
+function resetToNull(item)
+{
+    item.innerHTML="";
+    for(var i=0;i<3;i++)
+        winningCombination[i]=-1;
+    item.classList.remove("blur","not-allowed","unhover","highlight-winning-square");
 }
-function highlightWinningSquares(item,index){
-    console.log("hi");
+function highlightWinningSquares(item,index)
+{
     if(index!=winningCombination[0] && index!=winningCombination[1] && index!=winningCombination[2])
-    {
         item.classList.add("blur","not-allowed","unhover");
-    } 
     else
-    {
         item.classList.add("highlight-winning-square");
-    }   
+}   
+function playerMoved(event)
+{
+    if(currentPlayer===1 && event.target.innerHTML==="")
+    {
+        event.target.innerHTML=player1;
+        currentPlayer++;
+        movesPlayed++;
+    }
+    else if(currentPlayer===2 && event.target.innerHTML===""){
+        event.target.innerHTML=player2;
+        currentPlayer--;
+        movesPlayed++;
+    }
+    if(movesPlayed>4 && checkIfWinnerExist())
+    {
+        var winner;
+        currentPlayer===2?winner="X wins!!":winner="O wins!!"; 
+        winnerDisplay.innerHTML=winner;
+        winningMessege.classList.add("show-winning-messege");
+        allSquares.forEach(highlightWinningSquares);
+    }
+    else if(movesPlayed===9)
+        declareDraw();
 }
-
-
-
-// some crap
-$(document).ready(function(){
-    var squares= $(".square");
-
-    var player1='X';
-    var player2='O';
-
-    var currentPlayer=1;
-   
-    squares.click(function(){
-        if(currentPlayer===1 && this.innerHTML==="")
-        {
-            this.innerHTML=player1;
-            currentPlayer++;
-            movesPlayed++;
-        }
-        else if(currentPlayer===2 && this.innerHTML===""){
-            this.innerHTML=player2;
-            currentPlayer--;
-            movesPlayed++;
-        }
-        if(checkIfWinnerExist())
-        {
-            var winner;
-            currentPlayer===2?winner="X wins!!":winner="O wins!!"; 
-            winnerDisplay.innerHTML=winner;
-            winningMessege.classList.add("show-winning-messege");
-            allSquares.forEach(highlightWinningSquares);
-        }
-        else if(movesPlayed===9)
-        {
-            declareDraw();
-        }
-    })
-    
-});
-
-
-
-function checkIfWinnerExist(){
-    var moves=Array.prototype.slice.call($('.square'));
+function checkIfWinnerExist()
+{
+    var moves=Array.prototype.slice.call(squares);
     var results=moves.map(function(square){
         return square.innerHTML;
     });
@@ -105,9 +99,10 @@ function checkIfWinnerExist(){
             winningCombination[2]=combo[2];
             return true;
         }
-        } else {
-            return false;
         }
+        else 
+            return false;
+        
     });
 }
 function declareDraw(){
@@ -115,3 +110,4 @@ function declareDraw(){
     winningMessege.classList.add("show-winning-messege");
     allSquares.forEach(highlightWinningSquares);
 }
+////////////////////////////////////////////////////////////////////////////
