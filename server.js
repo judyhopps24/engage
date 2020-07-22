8b114a8671a0a55e5f455589ecde5df31f3ef4da
sristi <emailsristi@gmail.com>
2020-07-22 21:24:34 +0530

    hi

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+var PORT=process.env.PORT || 4000;
 var express=require('express');
 var socket= require('socket.io');
 //app setup
 var app=express();
-var server=app.listen(4000,function(){
+var server=app.listen(PORT,function(){
     console.log('listening on port 4000');
 });
 
